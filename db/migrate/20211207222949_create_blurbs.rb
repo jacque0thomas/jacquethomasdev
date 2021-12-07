f9f8ca48a4f852604dbb7c23a605bcc64c5b4b92
Jacque <jacque.0.thomas@gmail.com>
2021-12-07 17:30:51 -0500

    added model Blurb

---
+++ db/migrate/20211207222949_create_blurbs.rb
@@ -0,0 +1,10 @@
+class CreateBlurbs < ActiveRecord::Migration[6.1]
+  def change
+    create_table :blurbs do |t|
+      t.string :title
+      t.text :body
+
+      t.timestamps
+    end
+  end
+end

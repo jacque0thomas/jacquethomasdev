750ccbe8130988f7ed1de8ba4f24a36861ac0ae2
Jacque <jacque.0.thomas@gmail.com>
2021-12-07 18:22:14 -0500

    added basic about me

--- app/controllers/aboutme_controller.rb
+++ app/controllers/aboutme_controller.rb
@@ -1,4 +1,9 @@
 class AboutmeController < ApplicationController
-  def about-me
+  def about
+    @blurb = Blurb.find(1)
+  end
+
+  def show
+    @blurb = Blurb.find(1)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,9 @@
 Rails.application.routes.draw do
-  get 'aboutme/about-me'
+  get 'aboutme/about'
   root "homepage#home"
 
   
-  get "/aboutme", to: "aboutme#about-me"
+  get "/aboutme", to: "aboutme#about"
 
   get "/homepage", to: "homepage#home"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
